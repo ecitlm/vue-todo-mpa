8a6221f86a46eafb7c71f5776715f8419bd6cbf7
ecitlm <ecitlm@163.com>
2019-07-21 22:30:55 +0800

    feat:add readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ### vue-todo cli3
->vue-cli3脚手架
+>基于vue-cli3脚手架升级封装、可拿来即用、已集成基本常用模块
 
-**已集成**
-
+1. `vue-router`集成、并模块化抽离‘
+2. `axios`网络请求封装、以及拦截器判断
+3. `vuex`集成
+4. `postcss`集成、`px转rem`
+5. `filter`拦截器封装并抽离
+6. `sessionStorage`集成
--- src/assets/js/base/storage.js
+++ src/assets/js/base/storage.js
@@ -4,13 +4,12 @@ export default {
     let actCode = window.actCode || 'noActCode'
     let data = {}
     data['key'] = value
-    console.log(window.userOpenId)
-    window.sessionStorage.setItem(actCode + window.userOpenId + '_' + key, JSON.stringify(data))
+    window.sessionStorage.setItem(actCode + key, JSON.stringify(data))
   },
   get (key) {
     if (arguments.length < 1) throw new Error('arguments\'s length is less than one')
     let actCode = window.actCode || 'noActCode'
-    let stringfyData = window.sessionStorage.getItem(actCode + window.userOpenId + '_' + key)
+    let stringfyData = window.sessionStorage.getItem(actCode + '_' + key)
     if (stringfyData) {
       return JSON.parse(stringfyData)['key']
     } else {
